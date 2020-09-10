12ff8b55950ec9ba1a27a56593bda68a9f906d44
Omar <umeraslamjs@github.com>
2020-09-09 19:37:11 -0700

    bills post and get apis

---
+++ app/config/db_config.js
@@ -0,0 +1,4 @@
+const mongodb = {
+  dburl: "mongodb://localhost:27017/scanDB",
+};
+module.exports = { mongodb };
--- app/config/stripe_config.js
+++ app/config/stripe_config.js
@@ -2,11 +2,8 @@ const stripe = {
     stripe_client_id: 'ca_EQLlbyCv6pJWBqS6uYWBpbyfAODjNcjo',
     stripe_secret_id: 'sk_test_8WONGxWo090F1C4Ggqm8nUn9',
     stripe_public_id: 'pk_test_mIEBlVIOKBx6lt0FzuyGdFgo'
-}
-
-module.
-exports = {
-    stripe
+};
 
 
-}
+
+module.exports = {stripe}
