696c0d2033e08ee6280265dbbc4265b47223b67f
sarfaraz91 <sarfaraz_malik11@hotmail.com>
2020-09-25 17:45:40 +0500

    sdadsa

--- app/config/db_config.js
+++ app/config/db_config.js
@@ -1,5 +1,5 @@
 const mongodb = {
-  dburl: "mongodb+srv://Testing12@cluster0.y5d0r.mongodb.net/scanDB?retryWrites=true&w=majority",
+  dburl: "mongodb+srv://Testing12:Testing12@cluster0.y5d0r.mongodb.net/scanDB?retryWrites=true&w=majority",
   // dburl: "mongodb://localhost:27017/scanDB",
 };
 
