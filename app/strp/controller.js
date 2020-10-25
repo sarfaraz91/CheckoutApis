3cb0adc2c6b6fc76934fb197c1c62fb0fb970644
Omar <umeraslamjs@github.com>
2020-10-26 04:04:44 +0500

    payment

--- app/strp/controller.js
+++ app/strp/controller.js
@@ -6,17 +6,14 @@ var app = express();
 var mongoose = require("mongoose"),
   Users = mongoose.model("users"),
   Bills = mongoose.model("bills");
-
-var FCM = require("fcm-node");
 const { ObjectId } = require("mongodb");
-var serverKey ="AAAAYb8wGQo:APA91bGYMxIy97vJjlKp_-uGQPjYbiiyhZUC9vPUD8ZjuGc0CEKm0rBvBLA2y7eKnPfG-fKhzDZ0PqKnsb40aQxJTt3Ey0hSslCzfxClq1Q0GnP2rynzxATBXIK-T0ImaKFPoJCFdfbh";
-var fcm = new FCM(serverKey);
+var request = require('request');
 
 const stripe = require("stripe")(stripe_config.stripe.stripe_secret_id);
 app.use(express.json());
 
 var fcmConstants = {
-  fcmToken:"cbjs9wMfQw6pWLxulH1uPa:APA91bH3p7bPpQwPJta_95j_EcxXrBf7mcJqOYlY9qHxXXWnZ_4-QQsv4go-BiUPyGpO85Uf2moh2ZqwnNjUesSZuh08ASwtY1kmC-gGrRUCJA5qXZbDf-G6kAMwWk5I30YTcuC1I2Og",
+  fcmToken:"AAAAYb8wGQo:APA91bGYMxIy97vJjlKp_-uGQPjYbiiyhZUC9vPUD8ZjuGc0CEKm0rBvBLA2y7eKnPfG-fKhzDZ0PqKnsb40aQxJTt3Ey0hSslCzfxClq1Q0GnP2rynzxATBXIK-T0ImaKFPoJCFdfbh",
   senderId: "419819428106",
   url: "https://fcm.googleapis.com/fcm/send",
 };
@@ -24,7 +21,7 @@ var fcmConstants = {
 const _stripe = (req, res) => {
   const billId=ObjectId(req.body.billId);
   const amountPaid=req.body.amountPaid;
- const fcmToken=req.body.fcmToken;
+ // const fcmToken=req.body.fcmToken;
 
 
   stripe.charges
@@ -37,7 +34,11 @@ const _stripe = (req, res) => {
       res.json({ message: "Payment Succeded" });
       Bills.aggregate([{$match:{_id:billId}},{$project:{result:{$subtract:["$amount", amountPaid]}}}]).then((data)=>{
         if(data[0].result<=0){
-          notifyUpdate(fcmToken);
+res.status(200).send({amountRemaining:data[0].result,status:'paid'})
+          // notifyUpdate(fcmToken);
+        }else{
+            res.status(200).send({amountRemaining:data[0].result,status:'unpaid'})
+
         }
         Bills.update( { _id: billId }, [ { $set: { amount: data[0].result} } ] ).then(rest=>{res.status(200).send(rest)})
 
@@ -53,6 +54,7 @@ const _stripe = (req, res) => {
 };
 
 const _notification = (req, res) => {
+  var notifyIds=[];
   // var ids = ['5f6d2cb9db1609001784ca8f', '5f6d3329db1609001784ca90'];
   var userIds = req.body.userIds;
   var billId = req.body.billId;
@@ -66,8 +68,10 @@ const _notification = (req, res) => {
       Users.find({ email: { $in: userIds } })
         .then((users) => {
           users.forEach((items) => {
-            notify(items.token, billDivided,billData[0].amount);
+        notifyIds.push(items.token);
+            // notify(items.token, billDivided,billData[0].amount);
           });
+          notify(notifyIds,billDivided,billData[0].amount);
           res.status(200).send();
         })
         .catch((err) => {
@@ -81,7 +85,9 @@ const _notification = (req, res) => {
       Users.find({ email: { $in: userIds } })
         .then((users) => {
           users.forEach((items) => {
-            notify(items.token, billData[0].amount,null);
+            notifyIds.push(items.token);
+
+            // notify(items.token, billData[0].amount,null);
           });
           res.status(200).send();
         })
@@ -97,6 +103,42 @@ const _notification = (req, res) => {
 };
 
 
+var notify=(notificationUsers,dividedBill,totalBill)=>{
+  var headers = {
+    "Authorization": 'key=' + fcmConstants.fcmToken,
+    'Content-Type': 'application/json',
+    'Sender': 'id=' + fcmConstants.senderId
+  };
+  var requestData = {
+    registration_ids:notificationUsers,
+    "priority": "high",
+    "content_available": true,
+    "notification": {
+      "title": 'Bill Payment',
+      "body": `You are requested to pay ${dividedBill}`,
+    },
+      "data": {
+          "dividedBill":dividedBill,
+          "totalBill":totalBill
+
+      },
+  };
+    request.post({
+      headers: headers,
+      url: fcmConstants.url,
+      body: JSON.stringify(requestData),
+      method: 'POST'
+    }, function (error, httpResponse, body) {
+
+   console.log(httpResponse)
+
+    });
+
+
+}
+
+
+/*
 var notify = (token, bill) => {
   var jsonToken = token;
   console.log(jsonToken);
@@ -125,8 +167,9 @@ var notify = (token, bill) => {
     }
   });
 
-}
+}*/
 
+/*
 var notify = (token, bill,totalbill) => {
   var jsonToken = JSON.stringify(token);
   console.log(jsonToken);
@@ -154,6 +197,7 @@ var notify = (token, bill,totalbill) => {
     }
   });
 };
+*/
 
 // var message = { //this may vary according to the message type (single recipient, multicast, topic, et cetera)
 //   to: 'cbjs9wMfQw6pWLxulH1uPa:APA91bH3p7bPpQwPJta_95j_EcxXrBf7mcJqOYlY9qHxXXWnZ_4-QQsv4go-BiUPyGpO85Uf2moh2ZqwnNjUesSZuh08ASwtY1kmC-gGrRUCJA5qXZbDf-G6kAMwWk5I30YTcuC1I2Og',
